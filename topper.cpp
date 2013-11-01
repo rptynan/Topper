b7aec6c5de66f3ef9c1c7f885a06dce5644246af
Richard Tynan <rptynan2@hotmail.com>
2013-11-01 00:26:09 +0000

    Added geomview, .off importing and cube model

--- topper.cpp
+++ topper.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/convex_hull_2.h>
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef K::Point_2 Point_2;
-int main()
-{
-  Point_2 points[5] = { Point_2(0,0), Point_2(10,0), Point_2(10,10), Point_2(6,5), Point_2(4,1) };
-  Point_2 result[5];
-  Point_2 *ptr = CGAL::convex_hull_2( points, points+5, result );
-  std::cout <<  ptr - result << " points on the convex hull" << std::endl;
-  return 0;
+#include <fstream>
+
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/IO/Geomview_stream.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+#include <CGAL/IO/Polyhedron_geomview_ostream.h>
+
+typedef CGAL::Simple_cartesian<double> Kernel;
+typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
+
+
+int main(int argc, char *argv[]){
+	
+	CGAL::Geomview_stream gview(CGAL::Bbox_3(-10,-10,-10,60,60,60));
+	gview.set_line_width(4);
+	gview.set_trace(true);
+	gview.set_bg_color(CGAL::Color(0, 200, 200));
+	//gview.clear();
+
+	std::ifstream modelin(argv[1]);
+	Polyhedron model;
+	modelin>>model;
+	
+	gview<<CGAL::RED;
+	gview<<model;
+
+	std::getchar();
+	return 0;
+
 }
