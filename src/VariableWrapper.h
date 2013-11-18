d31bf94d5559ba6ada8dbccd3fe3f703b2c606b0
Richard Tynan <rptynan2@hotmail.com>
2013-11-18 01:27:15 +0000

    Complete overhaul of paths, should work now

--- src/VariableWrapper.h
+++ src/VariableWrapper.h
@@ -1,13 +1,13 @@
-#define BUFSIZE 256
+#define BUFSIZE 1024
 
 namespace VariableWrapper
 {
 
-	void Write_to_openscad(std::string path,double bbox[2][3], char modelpath[], int infillmode){
+	void Write_to_openscad(char path[],double bbox[2][3], char modelpath[], int infillmode){
 
-		path.replace(path.begin()+path.find("topper"),path.end(),"variables.scad");
-
-		std::ofstream varout(path.c_str());
+		strcat(path,"variables.scad");
+		std::ofstream varout(path);
+		std::cout<<"Absolute path to variables.scad: "<<path<<std::endl;
 		char buffer[BUFSIZE];
 
 		snprintf(buffer,BUFSIZE,"bbox = [[%f,%f,%f],[%f,%f,%f]];",bbox[0][0], bbox[0][1], bbox[0][2], bbox[1][0], bbox[1][1], bbox[1][2]);
--- src/topper.cpp
+++ src/topper.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 
 #include "VariableWrapper.h"
 
@@ -16,20 +17,39 @@ typedef	Polyhedron::Vertex_iterator Vertex_iterator;
 
 int main(int argc, char *argv[]){
 	
+
+//Input and path findings
+	//Start and checks
+	char abspathuser[BUFSIZE], abspathmodel[BUFSIZE], abspathexec[BUFSIZE];
 	if( argc<2 ){
 		std::cout<<"Model path not specified"<<'\n';
 		return 0;
 	}
-	std::ifstream modelin(argv[1]);
-	Polyhedron model;
-	modelin>>model;
-	
+	//User's pwd
+	realpath(".",abspathuser);
+	strcat(abspathuser,"/");
+	std::cout<<"Absolute path to user's directory: "<<abspathuser<<std::endl;
+	//Model's path
+	strcpy(abspathmodel,abspathuser);
+	strcat(abspathmodel,argv[1]);
+	std::cout<<"Absolute path to model: "<<abspathmodel<<std::endl;
+	//Executable's path
+	strcpy(abspathexec,abspathuser);
+	strcat(abspathexec,argv[0]);
+	abspathexec[strlen(abspathexec)-6]='\0';
+	std::cout<<"Absolute path to executable's directory: "<<abspathexec<<std::endl;
+	//More checks
 	if( argc<3 ){
 		std::cout<<"Infill not specified"<<'\n';
 		return 0;
 	}
 	int infillmode = atoi(argv[2]);
 
+
+//Loading model into CGAL
+	std::ifstream modelin(abspathmodel);
+	Polyhedron model;
+	modelin>>model;
 	double bbox[2][3];	//bbox[min,max][x,y,z]
 	for( Vertex_iterator v = model.vertices_begin(); v != model.vertices_end(); ++v ){
 		if( v->point().x() < bbox[0][0] ) bbox[0][0] = v->point().x();
@@ -39,14 +59,20 @@ int main(int argc, char *argv[]){
 		if( v->point().y() > bbox[1][1] ) bbox[1][1] = v->point().y();
 		if( v->point().z() > bbox[1][2] ) bbox[1][2] = v->point().z();
 	}
-	
+
+
+//Starting Geomview
 	CGAL::Geomview_stream gview(CGAL::Bbox_3(bbox[0][0],bbox[0][1],bbox[0][2],bbox[1][0],bbox[1][1],bbox[1][2]));
 	gview.set_bg_color(CGAL::Color(0, 200, 200));
 	//gview.clear();
 	gview<<CGAL::VIOLET<<model;
 
-	VariableWrapper::Write_to_openscad(std::string(argv[0]),bbox,argv[1],infillmode);
 
+//Transfer to OpenSCAD
+	VariableWrapper::Write_to_openscad(abspathexec,bbox,abspathmodel,infillmode);
+
+
+//Pause at end
 	std::getchar();
 	return 0;
 
